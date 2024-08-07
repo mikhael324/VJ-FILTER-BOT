f3540804ef47cc11616e532a1ce6378b01d6f8b8
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-08-07 10:57:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '5732c2ccb0386c692ab8d51275118f503a
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "10")
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "8085")
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
